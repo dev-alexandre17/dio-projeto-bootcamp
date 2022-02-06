169d4bbd7f3168b18e04e06b5229ec8db8c421ad
dev-alexandre17 <alexandretec77@gmail.com>
2022-02-06 15:44:52 -0300

    Iniciando classes Main, Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,46 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+
+    private String titulo;
+    private String descricao;
+    private LocalDate date;
+
+    public Mentoria() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
